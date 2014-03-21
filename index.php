2b78ce8271cafdcecf372d6a920b280e42d84c49
ehime <dodomeki@gmail.com>
2014-03-20 17:04:41 -0700

    Adding setup for conf

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-<?php echo "working";
+<?php
+
+require 'src/ValidUTF8XMLFilter.php';
+
+$doc = simplexml_load_file("php://filter/read=xmlutf8/resource=data/fmpxmlresult2.xml");
+
+echo $doc->asXML();
